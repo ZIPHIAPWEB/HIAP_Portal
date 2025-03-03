4499ed2192c9f0f5582ea65f5e2373c6bd3737a2
Renz Mergenio <xurpas@Users-MacBook-Pro.local>
2025-03-03 09:32:22 +0800

    fix: update footer social links

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -2,22 +2,32 @@
     <div class="container">
         <div class="row mb-3">
             <div class="col">
-                <p class="text-center m-0 p-3">2F University Center Building, 1985 C.M. Recto Avenue Sampaloc, Manila, 1008<br>09175228213 | 09228768213 |&nbsp;09175228213 | 09178988213 | 09084698962</p>
+                <p class="text-center m-0 p-3">2F University Center Building, 1985 C.M. Recto Avenue, Sampaloc, Manila, 1008<br>(02)83679392 | 0939-201-9198 | 0917-522-8213</p>
             </div>
         </div>
         <div class="row no-gutters justify-content-center mb-3">
-            <div class="col-12 col-xl-auto d-flex justify-content-center align-items-center"><a class="text-center flex-fill" href="https://www.facebook.com/HospitalityInstituteofAmericaPhilippines" style="width: 315px;" target="_blank"><i class="fa fa-facebook mr-1"></i>Hospitality Institute of America Philippines Inc.</a></div>
-            <div
-                class="col-12 col-xl-auto d-flex justify-content-center align-items-center"><a class="text-center flex-fill" href="https://www.facebook.com/HIAPLearningCenter" style="width: 173px;" target="_blank"><i class="fa fa-facebook mr-1"></i>HIAP - Learning Center</a></div>
-        <div class="col-12 col-xl-auto d-flex justify-content-center align-items-center"><a class="text-center flex-fill" href="https://www.ziptravel.com.ph/" target="_blank"><i class="fa fa-globe mr-1"></i>Zip Travel</a></div>
-        <div class="col-12 col-xl-auto d-flex justify-content-center align-items-center"><a class="text-center flex-fill" href="https://www.tesda.gov.ph/" target="_blank"><i class="fa fa-globe mr-1"></i>TESDA</a></div>
-        <div class="col-12 col-xl-auto d-flex justify-content-center align-items-center"><a class="text-center flex-fill" href="http://owwa.gov.ph/" target="_blank"><i class="fa fa-globe mr-1"></i>OWWA</a></div>
-    </div>
-    <div class="row">
-        <div class="col d-flex flex-column justify-content-center align-items-center flex-md-row flex-lg-row">
-            <p class="text-center m-0 p-1">Copyright 2020&nbsp;</p>
-            <p class="text-center m-0 p-1">Hospitality Institute of America-Philippines Inc.</p>
+            <div class="col-12 col-xl-auto d-flex justify-content-center align-items-center">
+                <a class="text-center flex-fill" href="https://www.facebook.com/HospitalityInstituteofAmericaPhilippines" style="width: 315px;" target="_blank">
+                    <i class="fa fa-facebook mr-1"></i>
+                    HIAP Inc. Facebook
+                </a>
+            </div>
+            <div class="col-12 col-xl-auto d-flex justify-content-center align-items-center">
+                <a class="text-center flex-fill" href="https://www.instagram.com/hiap.inc/" style="width: 173px;" target="_blank"><i class="fa fa-instagram mr-1"></i>
+                    HIAP Inc. Instagram
+                </a>
+            </div>
+            <div class="col-12 col-xl-auto d-flex justify-content-center align-items-center">
+                <a class="text-center flex-fill" href="https://linkedin.com/company/hiapinc" target="_blank"><i class="fa fa-linkedin mr-1"></i>
+                    HIAP Inc. LinkedIn
+                </a>
+            </div>
         </div>
-    </div>
+        <div class="row">
+            <div class="col d-flex flex-column justify-content-center align-items-center flex-md-row flex-lg-row">
+                <p class="text-center m-0 p-1">Copyright @ {{ date('Y') }}</p>
+                <p class="text-center m-0 p-1">Hospitality Institute of America-Philippines Inc.</p>
+            </div>
+        </div>
     </div>
 </footer>
